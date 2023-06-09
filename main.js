0acfcf371d823b1fecf92758372587b2f19d7146
dezoul <aussieme@gmail.com>
2023-06-09 16:21:07 +0800

    added login

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-$('.hamburger-button').click(function(){
-    $('.mobile-menu').fadeToggle(100);
-    $(this).toggleClass('active');
-});
+
+const burgerMenu = document.querySelector('.mobile-menu')
+const burgerButton = document.querySelector('.hamburger-button')
+
+
+burgerButton.addEventListener('click',(e) => {
+    e.defaultPrevented
+    e.target.classList.toggle('active')
+    let menuHeight =  e.target.classList.contains('active') ? '100vh':'0vh'
+
+    if (e.target.classList.contains('active')){
+        gsap.to(burgerMenu, {
+            height: '100vh',
+            padding: "5%",
+        })
+    } else {
+        gsap.to(burgerMenu, {
+            height: '0vh',
+            padding: '0',
+        })
+    }
+})
+
+
